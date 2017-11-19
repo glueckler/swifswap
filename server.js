78676674a9fde9ad42c165fa53eeafe4b397802b
Thomas Nicholson <nicholmen@gmail.com>
2017-11-18 17:04:14 -0800

    wrote  out most  basic routes skeletons

--- server.js
+++ server.js
@@ -47,7 +47,7 @@ api.delete('users/:id', async ctx => {
 })
 
 
-//session routes
+//SESSION ROUTES
 //Login
 api.post('/session', async ctx => {
   ctx.body = 'you called the post method at /session'
@@ -57,9 +57,35 @@ api.delete('/session', async ctx => {
   ctx.body = 'you called the delete method at /session'
 })
 
-//item routes
+//ITEM ROUTES
+//see item
 api.get('/items/:id', async ctx => {
   ctx.body = 'you called get at /items/:id'
 })
+//create item
+api.post('/items', async ctx => {
+  ctx.body = 'you called post at /items'
+})
+//update item
+api.post('/items/:id', async ctx => {
+  ctx.body = 'you called update at /items/:id'
+})
+//delete item
+api.delete('/items/:id', async ctx => {
+  ctx.body = 'you called delete at /items/:id'
+})
 
+//CHAT ROUTES
+// Get chat rooms
+api.get('/chats', async ctx => {
+  ctx.body = 'you called get at /chats'
+})
+//Create new chat
+api.post('/chats/:id', async ctx => {
+  ctx.body = 'you called post at /chats/:id'
+})
+//Delete chat
+api.delete('/chats/:id', async ctx => {
+  ctx.body = 'you called delete at /chats/:id'
+})
 app.listen(3000);

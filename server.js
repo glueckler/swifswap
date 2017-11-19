a7c54f57a4f7aefc69d9fb85ef649077093ff706
Thomas Nicholson <nicholmen@gmail.com>
2017-11-18 16:13:42 -0800

    added some routes to server.js, committing to get synchronized before branching off to a development git branch

--- server.js
+++ server.js
@@ -27,5 +27,38 @@ api.get('/', async ctx => {
   ctx.body = 'Hello mr Mr'
 })
 
+//USERS ROUTES
+// See user profile
+api.get('/users/:id', async ctx => {
+  ctx.body = 'you are in the users id route'
+})
+//Create new user 
+api.post('/users', async ctx => {
+  ctx.body = 'you called the post method at /users'
+})
+//Update user profile 
+api.put('/users/:id', async ctx => {
+  ctx.body = 'you called the put method at /users/:id'
+})
+//delete account 
+api.delete('users/:id', async ctx => {
+  ctx.body = 'you called the delete method at /users/:id'
+})
+
+
+//session routes
+//Login
+api.post('/session', async ctx => {
+  ctx.body = 'you called the post method at /session'
+})
+//Logout
+api.delete('/session', async ctx => {
+  ctx.body = 'you called the delete method at /session'
+})
+
+//item routes
+api.get('/items/:id', async ctx => {
+  ctx.body = 'you called get at /items/:id'
+})
 
 app.listen(3000);

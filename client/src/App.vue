1a3a45dd5f462f6679d3e9e0b508ff86ca343746
Thomas Nicholson <nicholmen@gmail.com>
2017-11-22 22:06:11 -0800

    adding more routes and login base

--- client/src/App.vue
+++ client/src/App.vue
@@ -2,10 +2,12 @@
   <div id="app">
     <nav class="nav">
       <ul class="nav__list">
-        <li>Login</li>
+        <li><router-link to="/">Swifswap</router-link></li>
+        <li><router-link to="/login">Login</router-link></li>
         <li><router-link to="/register">Register</router-link></li>
         <li>Chat</li>
         <li>Profile</li>
+        <li>Chats</li>
       </ul>
       <router-view/>
     </nav>
--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,11 +1,14 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Register from '../components/register.vue'
+import Login from '../components/login.vue'
 
 Vue.use(Router)
 
 const routes = [
-  { path: '/register', component: Register }
+  { path: '/register', component: Register },
+  { path: '/login', component: Login}  
+
 ]
 
 

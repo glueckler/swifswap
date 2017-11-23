e2aad614fd182a116e7736658e9319e3fc04d843
Thomas Nicholson <nicholmen@gmail.com>
2017-11-22 22:05:22 -0800

    adding more routes

---
+++ client/src/components/login.vue
@@ -0,0 +1,37 @@
+<template>
+  <form class="login">
+      <label for="username-email">Username or Email</label>
+      <input
+        type="text" 
+        id="username-email"
+        v-model="username">
+      <label for="password">Password</label>
+      <input
+        type="password" 
+        id="password"
+        v-model="password">
+    <button type="submit">Register</button>
+  </form>  
+</template>
+
+<script>
+export default {
+  name: 'login',
+  data() {
+    return {
+      username: '',
+      email: '',
+      password: ''
+    }
+  },
+  methods: {
+    login() {
+      
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
